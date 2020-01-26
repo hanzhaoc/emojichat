9f53baabef2ddeb5adb2c7d8594746cb30df9624
Hanzhao Chen <hanzhao.chen@appfolio.com>
2020-01-25 16:31:39 -0800

    use gift chat pacakge

--- EmojiChat/App.js
+++ EmojiChat/App.js
@@ -1,23 +1,6 @@
-import React, { useEffect } from 'react';
-import { StyleSheet } from 'react-native';
+import React from 'react';
 import HomeScreen from './screens/HomeScreen';
-import io from 'socket.io-client';
-
 export default function App() {
-  useEffect(() => {
-    io("http://172.16.77.11:3001")
-  },[])
-
   console.disableYellowBox = true;
-
   return (<HomeScreen />);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- EmojiChat/screens/HomeScreen.js
+++ EmojiChat/screens/HomeScreen.js
@@ -1,46 +1,44 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
 import io from 'socket.io-client'
-export default function HomeScreen() {
+import { GiftedChat } from 'react-native-gifted-chat'
 
-  const [messageToSend, setMessageToSend] = useState("");
+export default function HomeScreen() {
   const [recvMessages, setRecvMessages] = useState([]);
   const socket = useRef(null)
 
   useEffect(() => {
-    socket.current = io("http://172.16.77.11:3001");
+    socket.current = io("http://192.168.5.74:3001");
     socket.current.on('message', message => {
-      setRecvMessages(prevState => [...prevState, message])
+      const testMessage = {  
+        _id: 3,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        }
+      };
+      testMessage.text = message;
+      setRecvMessages(prevState => GiftedChat.append(prevState, testMessage))
     })
   },[])
 
-  const sendMessage = () => {
-    socket.current.emit('message', messageToSend)
-    setMessageToSend('');
+  const onSend = (messages) => {
+    // console.log(messages);
+    // console.log(messages[0]);
+    socket.current.emit('message', messages[0].text)
   }
 
-  const receivedMessages = recvMessages.map( message => {
-    return <Text key={message}>{message}</Text>
-  })
-
   return (
-    <View style={styles.container}>
-      {receivedMessages}
-      <TextInput 
-      value={messageToSend} 
-      onChangeText={text => setMessageToSend(text)}
-      placeholder="Enter chat message..."
-      onSubmitEditing={sendMessage}
+      <GiftedChat
+        messages={recvMessages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        }}
       />
-    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
